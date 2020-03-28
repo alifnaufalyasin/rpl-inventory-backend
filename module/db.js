f4c3c545c39c89142186755e21d182b1a467ecc3
Alif Naufal <alifnaufalyasin@gmail.com>
2020-03-28 16:35:48 +0700

    UPDATE

--- module/db.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const MongoClient = require('mongodb').MongoClient;
-
-const uri = "mongodb+srv://admin:"+process.env.passDB+"@alive-t50rd.mongodb.net/test?retryWrites=true&w=majority"
-const client = new MongoClient(uri, { useUnifiedTopology: true })
-
-module.exports = {
-  client
-}
