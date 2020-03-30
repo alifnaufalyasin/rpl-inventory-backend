06f8310eb46cc8be00a5f7605c686c15a73e0ce7
Alif Naufal <alifnaufalyasin@gmail.com>
2020-03-30 19:17:38 +0700

    UPDATE

--- module/auth.js
+++ module/auth.js
@@ -5,11 +5,6 @@ function signUser(user) {
   return jwt.sign(user, process.env.tokenSecret, { expiresIn: 60*10})
 }
 
-function authToken(req,res,next) {
-  
-}
-
 module.exports = {
-  signUser,
-  authToken
+  signUser
 }
