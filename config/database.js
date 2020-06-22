26a4bea38db4710a444f97469bbb70eef92cf430
alifnaufalyasin <alifnaufal171199@gmail.com>
2020-06-22 23:51:36 +0700

    Update

--- config/database.js
+++ config/database.js
@@ -6,7 +6,11 @@ const db = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host : process.env.DB_HOST,
-    dialect : 'mysql'
+    dialect : 'mysql',
+    dialectOptions: {
+      useUTC: false //for reading from database
+    },
+    timezone: "+07:00" //for writing to database
   }
 )
 
